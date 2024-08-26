9e0d38b9789fe5663ccb9ae76ef1a587a1368faf
vriquelmefe <vriquelmefe@gmail.com>
2024-08-26 00:09:20 -0400

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,28 @@
+import Navegacion from './componentes/Navegacion.jsx'
+import Home from './componentes/Home.jsx'
+import Footer from './componentes/Footer.jsx'
+import Login from './componentes/Login.jsx'
+import Registro from './componentes/Registro.jsx'
+import Cart from './componentes/Cart.jsx'
+import './App.css'
+import { useState } from 'react';
+
+
+function App() {
+  const [currentView,setCurrentView] = useState('home');
+  
+  const handleShowCart = () => {setCurrentView('cart');};
+  return (
+    <>
+      <Navegacion onCartClick={handleShowCart} />
+       {currentView === 'cart' && <Cart/>}
+      <Home />
+   <Login />
+      <Registro />
+      <Cart/>
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/componentes/Grilla.jsx
@@ -0,0 +1,22 @@
+import CardPizza from "./CardPizza"
+import pizzas from "../assets/pizzas"
+const Grilla = () => {
+    return (
+          <div className="container productos">
+            <div className="row ms-auto" >
+                {pizzas.map((pizza) => (
+                    <div className="col-md-4" key={pizza.id}>
+                        <CardPizza
+                            img={pizza.img}
+                            name={pizza.name}
+                            price={pizza.price}
+                            ingredientes={pizza.ingredients.join(' , ')}
+                        />
+                    </div>
+                ))}
+               
+            </div>
+            </div>
+    )
+}
+export default Grilla
