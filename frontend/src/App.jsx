2335617015d501ed99d414ff3e2501e47026b10b
vriquelmefe <vriquelmefe@gmail.com>
2024-08-26 00:57:26 -0400

    fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,37 +5,19 @@ import Footer from './componentes/Footer.jsx'
 // import Registro from './componentes/Registro.jsx'
 import Cart from './componentes/Cart.jsx'
 import './App.css'
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Pizza from './componentes/Pizza.jsx'
 
 
 function App() {
   const [currentView, setCurrentView] = useState('home');
-  const [pizza, setPizza] = useState('');
-  
-  
-  //Consumo de API
-  const urlPizza = "http://localhost:5001/api/pizzas/"
-
-  const getData = async () => {
-    const response = await fetch(urlPizza);
-    const dataPizzas = await response.json();
-
-    console.log('dataPizzas'   , dataPizzas);  // console.log para ver los datos en consola
-    setPizza(dataPizzas)
-   
-  };
-
-  useEffect(() => {
-    getData();
-  }, []);
   
   const handleShowCart = () => {setCurrentView('cart');};
   return (
     <>
       <Navegacion onCartClick={handleShowCart} />
        {currentView === 'cart' && <Cart/>}
-      <Home pizza={pizza} />
+      <Home  />
    {/* <Login />
       <Registro />
       <Cart/> */}
--- frontend/src/componentes/Home.jsx
+++ frontend/src/componentes/Home.jsx
@@ -1,11 +1,30 @@
 import Grilla from "./Grilla";
 import Header from "./Header"
+import { useState,useEffect } from "react";
 
 const Home = () => {
+    const [pizza, setPizza] = useState('');
+  
+  
+  //Consumo de API
+  const urlPizza = "http://localhost:5001/api/pizzas/"
+
+  const getData = async () => {
+    const response = await fetch(urlPizza);
+    const dataPizzas = await response.json();
+
+    console.log('dataPizzas'   , dataPizzas);  // console.log para ver los datos en consola
+    setPizza(dataPizzas)
+   
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
     return (
         <>
             <Header titulo="Mamma Mia" descripcion="Tenemos las mejores pizzas que podrás encontrar"/>
-          <Grilla/>
+          <Grilla pizza={pizza}/>
         </>
     )
 }
